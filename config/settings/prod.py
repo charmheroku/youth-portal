c7ef211f63e71e509e684d80726439873d2bfb31
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-07 16:41:00 +0100

    refactor(settings): split settings into base/dev/prod

---
+++ config/settings/prod.py
@@ -0,0 +1,16 @@
+import os
+from .base import *  # noqa: F403, F401
+
+DEBUG = False
+ALLOWED_HOSTS = ["mydomain.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
