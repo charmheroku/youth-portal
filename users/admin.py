dd7ee26dc5dc45c6cc4356133bc331dc958d768a
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-08 17:02:45 +0100

    feat(admin): register CustomUser in Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Custom user admin panel."""
+
+    model = CustomUser
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "gender")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "phone",
+                    "birth_date",
+                    "gender",
+                    "telegram",
+                    "avatar",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "first_name", "last_name")
