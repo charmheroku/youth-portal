6964a8ba6a435bb9d842298f6b4184cccd97266c
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-08 15:20:30 +0100

    feat(users): create CustomUser model with email authentication

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model with email as the unique identifier instead of username.
+
+    """
+
+    email = models.EmailField(unique=True)
+    username = None
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=[("male", "Male"), ("female", "Female")],
+        blank=True,
+    )
+    telegram = models.CharField(max_length=100, blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+    ]
+
+    def __str__(self):
+        return self.email
