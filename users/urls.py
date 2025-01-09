89fbca0ad88e8e6963988bfb658ffb4fea5da53e
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-09 23:08:25 +0100

    feat(auth): add login, logout, and authentication templates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from users.views import UserSignUpView
+from users.views import ProfileView, UserSignUpView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
+    path("profile/", ProfileView.as_view(), name="profile"),
     path("signup/", UserSignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
+
+app_name = "users"
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from users.models import CustomUser
 from users.forms import SignUpForm
 
@@ -11,10 +12,26 @@ class UserSignUpView(CreateView):
     model = CustomUser
     form_class = SignUpForm
     template_name = "users/signup.html"
-    success_url = "/"
+    success_url = reverse_lazy("users:profile")
 
     def form_valid(self, form):
         """Log in user after successful signup"""
         self.object = form.save()
         login(self.request, self.object)
         return super().form_valid(form)
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    """
+    Profile page for authenticated users.
+    """
+
+    model = CustomUser
+    template_name = "users/profile.html"
+    context_object_name = "user_profile"
+
+    def get_object(self):
+        """
+        Returns the currently logged-in user as the object.
+        """
+        return self.request.user
