03139cd5e4c0b589e1e78f0bdbee1cc28d76d7a4
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-10 15:57:48 +0100

    feat(books): implement CRUD views and forms for Book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from .models import Book
+from .forms import BookForm
 
-# Create your views here.
+
+class BookListView(ListView):
+    """
+    Displays the list of all books.
+    """
+
+    model = Book
+    template_name = "books/book_list.html"
+    context_object_name = "books"
+
+
+class BookDetailView(DetailView):
+    """
+    Shows details of a specific book, including cover and description.
+    """
+
+    model = Book
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
+
+
+class BookCreateView(CreateView):
+    """
+    Allows admin (or staff) to create a new book.
+    """
+
+    model = Book
+    form_class = BookForm
+    template_name = "books/book_form.html"
+    success_url = reverse_lazy("books:book_list")
+
+
+class BookUpdateView(UpdateView):
+    """
+    Allows admin (or staff) to edit an existing book.
+    """
+
+    model = Book
+    form_class = BookForm
+    template_name = "books/book_form.html"
+    success_url = reverse_lazy("books:book_list")
+
+
+class BookDeleteView(DeleteView):
+    """
+    Allows admin (or staff) to delete a book.
+    """
+
+    model = Book
+    template_name = "books/book_confirm_delete.html"
+    success_url = reverse_lazy("books:book_list")
--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("books/", include("books.urls", namespace="books")),
 ]
 
 if settings.DEBUG:
