2dc6d22d757cf130f4327e9ff9749793fc40129d
Oleksii Ivanov <oleks4335@gmail.com>
2025-01-11 11:37:14 +0100

    refactor(mixins): explicitly require authentication in AdminRequiredMixin

--- books/mixins.py
+++ books/mixins.py
@@ -1,10 +1,18 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
-class AdminRequiredMixin(UserPassesTestMixin):
+class AdminRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
     """
-    Custom mixin to allow access only for staff users.
+    Custom mixin to restrict access to admin users only.
     """
 
     def test_func(self):
         return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        messages.warning(
+            self.request, "You do not have permission to access this page."
+        )
+        return redirect("books:list")
